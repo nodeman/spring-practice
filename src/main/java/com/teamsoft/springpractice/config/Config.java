55855aa59f038aac9eab630edb2a9b1f503255c0
Jay <nodeman@icloud.com>
2022-02-14 15:41:01 +0900

    Config #1 - 스프링 Config 설정 및 적용
    스프링 Config 설정 및 적용

---
+++ src/main/java/com/teamsoft/springpractice/config/Config.java
@@ -0,0 +1,9 @@
+package com.teamsoft.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.teamsoft.springpractice")
+@Configuration
+public class Config {
+}
